4a3186eaac79892e982d7455f97c44155c6894f9
filedesless <filedesless@gmail.com>
2017-12-14 18:10:41 -0500

    Finished day 4

--- 4/sol.py
+++ 4/sol.py
@@ -7,4 +7,12 @@ for line in open('input'):
     if sorted(arr) == sorted(list(set(arr))):
         total += 1
 
-print(total)
+print("P1: ", total)
+
+total = 0
+for line in open('input'):
+    bag = set(["".join(sorted(word)) for word in line.split()])
+    if len(bag) == len(line.split()):
+        total += 1
+
+print("P2: ", total)
